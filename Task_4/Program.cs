4170f1fd647b48f25a80c6bf75338f9a41907612
vlad <norg.vl@mail.ru>
2023-01-27 16:06:21 +0300

    Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+int a = new Random().Next(-10,10);
+int b = new Random().Next(-10,10);
+int c = new Random().Next(-10,10);
+Console.WriteLine("Числа: "+a+" "+b+" "+c);
+int max = a;
+
+if (max<b)
+{
+    max = b;
+}
+
+if (max < c)
+{
+    max = c;
+}
+
+Console.WriteLine("Максимальное число: "+max);
+
+
