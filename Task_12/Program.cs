5531eaccd92b904e1d7b4f50d641c6e9c5ddb11b
vlad <norg.vl@mail.ru>
2023-01-31 18:16:02 +0300

    seminar2 ready

---
+++ Task_12/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. 
+Если второе число не кратно числу первому, то программа выводит остаток от деления. */
+Console.Clear();
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+if (b%a == 0)
+{
+    Console.WriteLine($"{b} kratno {a}");
+}
+else
+{
+    Console.WriteLine($"Ne kratno, ostatok: {b%a}");
+}
---
+++ Task_16/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого. */
+
+Console.Clear();
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+if (a*a == b)
+{
+    Console.WriteLine($"b = {b} kvadrat a = {a}");
+}
+else if (b*b == a)
+{
+    Console.WriteLine($"a = {a} kvadrat b = {b}");
+}
+else
+{
+    Console.WriteLine("Ne kvadrati");
+}
