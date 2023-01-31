30b95a74560d6f3b856aa681a43a2516864c325a
vlad <norg.vl@mail.ru>
2023-01-31 15:09:43 +0300

    New Tasks seminar2

---
+++ Task_11/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+int a = new Random().Next(100, 1000);
+
+int[] Splitting(int a)
+{
+    int[] arr = new int[3];
+    int a11 = a / 10;     // десятки
+    int a21 = a % 100;     // единицы
+    arr[0] = a / 100;     // сотни
+    arr[1] = a11 - arr[0] * 10; // десятки
+    arr[2] = a21 - arr[1] * 10;  // единицы
+    return arr;
+
+}
+
+
+int[] arr = Splitting(a);
+Console.WriteLine(a + " " + arr[0] + " " + arr[1] + " " + arr[2]);
+Console.WriteLine(arr[0]+""+arr[2]);
---
+++ Task_9/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+int a = new Random().Next(100, 1000);
+
+int[] Splitting(int a)
+{
+    int[] arr = new int[3];
+    int a11 = a / 10;     // десятки
+    int a21 = a % 100;     // единицы
+    arr[0] = a / 100;     // сотни
+    arr[1] = a11 - arr[0] * 10; // десятки
+    arr[2] = a21 - arr[1] * 10;  // единицы
+    Console.WriteLine(a + " " + arr[0] + " " + arr[1] + " " + arr[2]);
+    return arr;
+
+}
+
+int FindMax(int[] arr)
+{
+    int max = arr[1];
+    if (arr[2] > max)
+    {
+        max = arr[2];
+    }
+    if (arr[0] > max)
+    {
+        max = arr[0];
+    }
+    return max;
+}
+
+int[] arr = Splitting(a);
+int max = FindMax(arr);
+
+Console.WriteLine(max);
