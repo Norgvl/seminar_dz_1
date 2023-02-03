9cb009810637b27e9e045e576ebb51d37cf97474
vlad <norg.vl@mail.ru>
2023-02-03 18:43:48 +0300

    end_of_sem3

---
+++ Task_20/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает координаты точек A и B и возвращает расстояние между ними в 2d пространстве
+// A(x1,y1) B(x2,y2)
+// sqrt((x1-x2)^2 + (y1 - y2)^2)
+
+Console.Clear();
+Console.Write("Enter A(x): ");
+double x1 = double.Parse(Console.ReadLine());
+Console.Write("Enter A(y): ");
+double y1 = double.Parse(Console.ReadLine());
+Console.Write("Enter A(z): ");
+double z1 = double.Parse(Console.ReadLine());
+Console.Write("Enter B(x): ");
+double x2 = double.Parse(Console.ReadLine());
+Console.Write("Enter B(y): ");
+double y2 = double.Parse(Console.ReadLine());
+Console.Write("Enter B(z): ");
+double z2 = double.Parse(Console.ReadLine());
+
+double dist = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+
+Console.WriteLine($"d = {dist:f3}");
---
+++ Task_22/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+Console.Clear();
+Console.Write("enter the number: ");
+int N = int.Parse(Console.ReadLine());
+if (N < 0)
+{
+    N = N * (-1);
+}
+else if (N == 0)
+{
+    Console.WriteLine("0");
+    return;
+}
+
+
+double[] arr = new double[N];
+
+void Count(double[] arr, int N)
+{
+
+    for (int i = 0; i < N; i++)
+    {
+        double a = i + 1;
+        arr[i] = Math.Pow(a, 2);
+    }
+
+}
+
+void PrintArray(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+
+Count(arr, N);
+PrintArray(arr);
